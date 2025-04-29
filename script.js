3cae6833f008aaeabc50720904acc17578e5c71e
Manolinhopng <rmanuelorrego@gmail.com>
2025-04-29 00:38:58 -0500

    Resueltos los conflictos

--- script.js
+++ script.js
@@ -5,7 +5,27 @@ document.addEventListener("DOMContentLoaded", function () {
         downloadBtn.addEventListener("click", function () {
             const element = document.body;
 
-<<<<<<< HEAD
+            const opt = {
+                margin: 0.5,
+                filename: "mi_cv.pdf",
+                image: {
+                    type: "jpeg",
+                    quality: 0.98
+                },
+                html2canvas: {
+                    scale: 2
+                },
+                jsPDF: {
+                    unit: "in",
+                    format: "letter",
+                    orientation: "portrait"
+                },
+            };
+
+            html2pdf().set(opt).from(element).save();
+        });
+    }
+
             html2canvas(element, {
                 scale: 2,
                 useCORS: true
@@ -42,33 +62,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error('Error al capturar la imagen:', error);
             });
         });
-    }
-});
 const contactForm = document.getElementById("contactForm");
 if (contactForm) {
-=======
-            const opt = {
-                margin: 0.5,
-                filename: "mi_cv.pdf",
-                image: {
-                    type: "jpeg",
-                    quality: 0.98
-                },
-                html2canvas: {
-                    scale: 2
-                },
-                jsPDF: {
-                    unit: "in",
-                    format: "letter",
-                    orientation: "portrait"
-                },
-            };
-
-            html2pdf().set(opt).from(element).save();
-        });
-    }
-
->>>>>>> 3d3ea9fdd9a0226c9f198360df5431e2f54c7437
     contactForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
@@ -102,8 +97,4 @@ if (contactForm) {
             submitButton.textContent = "Enviar";
         });
     });
-<<<<<<< HEAD
-}
-=======
-});
->>>>>>> 3d3ea9fdd9a0226c9f198360df5431e2f54c7437
+};
